4dd4c70170e2cc196c22e9731827f6a97178a620
Alessandra-Casu <alessandracasu919@gmail.com>
2023-05-18 17:21:10 +0200

    add casino

--- src/App.vue
+++ src/App.vue
@@ -2,7 +2,7 @@
 import AppHeader from "./components/AppHeader.vue";
 import AppMain from "./components/AppMain.vue";
 import AppFooter from "./components/AppFooter.vue";
-import AppBlackRow from "./components/AppSBlackRow.vue";
+import AppBlackRow from "./components/AppBlackRow.vue";
 import AppMenu from "./components/AppMenu.vue";
 
 export default {
