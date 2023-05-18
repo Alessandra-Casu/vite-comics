291c5c056bfb6620171bd07a9266dc2f57650b7b
Alessandra-Casu <alessandracasu919@gmail.com>
2023-05-18 14:42:18 +0200

    add struct

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,9 @@
+<script></script>
+
+<template>
+  <div>
+    <h2>Io sono il Footer</h2>
+  </div>
+</template>
+
+<style lang="scss" update></style>
---
+++ src/components/AppMain.vue
@@ -0,0 +1,9 @@
+<script></script>
+
+<template>
+  <div>
+    <h2>Io sono il Main</h2>
+  </div>
+</template>
+
+<style lang="scss" update></style>
