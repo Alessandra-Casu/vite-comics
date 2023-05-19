9612b1761c6339de804a758d4f08f7f6511e4833
Alessandra-Casu <alessandracasu919@gmail.com>
2023-05-19 17:09:42 +0200

    add card

---
+++ src/components/CardComics.vue
@@ -0,0 +1,54 @@
+<script>
+export default {
+  props: {
+    thumbImg: {
+      type: String,
+      required: false,
+      default: "indirizzo immagine di default",
+    },
+    price: {
+      type: String,
+      required: true,
+    },
+    series: {
+      type: String,
+      required: false,
+    },
+    type: {
+      type: String,
+      required: false,
+    },
+  },
+};
+</script>
+
+<template>
+  <div class="cards">
+    <div class="card">
+      <img :src="thumbImg" :alt="price" />
+      <h2>{{ series }}</h2>
+      <h3>{{ type }}</h3>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" update>
+.cards {
+  width: 70%;
+  margin: auto;
+  display: flex;
+}
+.card {
+  width: 30%;
+
+  text-align: center;
+  border: 2px solid salmon;
+  background-color: gray;
+  color: black;
+
+  img {
+    width: 60px;
+    height: 60px;
+  }
+}
+</style>
